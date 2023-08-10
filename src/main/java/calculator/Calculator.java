84b2f5913cecbb67b403cb0858391a7572bcf033
raadted <a.ahmadi.k.79@gmail.com>
2023-08-10 12:20:31 +0330

    feat: Add new calculator functions

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,22 @@
 package calculator;
 
+
+import static java.lang.Math.pow;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public int power(int a, int b) {
+        return (int)pow(a, b);
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
     }
 }
